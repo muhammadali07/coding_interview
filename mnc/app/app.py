5aaa8fa21ff84461b87d3f5c6a89066b8c3676dc
Muh Ali Bakhtiar <36842581+muhammadali07@users.noreply.github.com>
2021-06-22 15:52:13 +0700

    update flask

--- mnc/app/app.py
+++ mnc/app/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
-import pymongo
+from flask import Flask, jsonify
 import os
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://192.168.99.100:27017/mplus"
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
     return 'hello word'
+
+@app.route('/check_data')
+def check_data():
+    data = mongo.db.category.find({})
+    for x in data:
+        return jsonify({'nama category':x['nama_category']})
 
 if __name__ =='__main__':
     app.run(debug=True)
---
+++ mnc/app/connection.py
@@ -0,0 +1,5 @@
+# from pymongo import MongoClient
+
+# connection = MongoClient('127.0.0.1', 27017)
+# db = connection.mnc
+
